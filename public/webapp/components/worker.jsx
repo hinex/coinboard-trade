50d47de1083bae682fddc9ad73823aaa3cfcf3d2
Roman Hinex <roman@hinex.org>
2017-02-20 00:20:04 +0600

    Add reconnect for socket.io client

--- public/webapp/components/worker.jsx
+++ public/webapp/components/worker.jsx
@@ -3,12 +3,19 @@ import CurrencyStore from '../stores/currency';
 import { observer } from 'mobx-react';
 import io from 'socket.io-client';
 
-const socket = io('/');
+const socket = io('/', {
+  reconnection: true,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax : 5000,
+  reconnectionAttempts: 100
+});
+
 const currency = new CurrencyStore();
 
 socket.on('connect', () => console.log('socket connected'));
+socket.on('reconnect', () => console.log('reconnecting...'));
+socket.on('disconnect', () => console.log('disconnected'));
 socket.on('hw', (data) => currency.hw = JSON.stringify(data));
-socket.on('disconnect', function(){});
 
 @observer class Worker extends React.Component {
   render() {
